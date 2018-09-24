d9b3918f7306bf3329847ba98c141aef515f3829
bbakernoaa <barry.baker@noaa.gov>
2018-09-24 11:49:25 -0400

    Updating to python3

--- trollbufr/coder/__init__.py
+++ trollbufr/coder/__init__.py
@@ -4,3 +4,11 @@ Modules/classes for reading a BUFR.
 
 No entry point for pybufr-API here.
 """
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
